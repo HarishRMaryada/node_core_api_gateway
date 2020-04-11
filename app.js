1a5ae5ec15910e36927cc21bf1d1e0488983095a
Harish <>
2020-04-11 22:35:50 +0530

    winston error logs and exception handling

--- app.js
+++ app.js
@@ -7,12 +7,47 @@ if (cluster.isMaster) {
   // cluster.fork() 
   // cluster.fork()
 } else {
+  //third party imports
   const config = require('config')
   const express = require('express')
   const helmet = require('helmet')
   const morgan = require('morgan')
+  const winston  = require('winston')
+
+
+  //application imports
+  const error = require('./middleware/error')
   const app = express()
+
+
+  process.on('uncaughtException',(ex)=>{
+    winston.error(ex.message,ex)
+  })
   
+  process.on('unhandledRejection',(ex)=>{
+    winston.error(ex.message,ex)
+  })
+
+  const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+      //
+      // - Write to all logs with level `info` and below to `combined.log` 
+      // - Write all logs error (and below) to `error.log`.
+      //
+      new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'logs/combined.log' })
+    ]
+  });
+
+
+  if(app.get('env') !== 'production'){
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple()
+    }));
+  }
 
   //calling api's end point need to simplify and organize
   app.use("/",require("./controllers"))
@@ -36,6 +71,9 @@ if (cluster.isMaster) {
   //develop on app db 
   const dbdebug = require('debug')('app:db')
   dbdebug("Db en")
+
+  //global error handler
+  app.use(error)
 
   
   const port =  process.env.PORT || 3000
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const asyncMiddleware = require('../middleware/async')
 
-router.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+
+
+router.get('/', asyncMiddleware(
+    (req, res) => {
+        throw new Error('Error with API call')
+        //res.send('Hello World!')
+        // try{
+        //    // res.send('Hello World!')
+        //     throw new Error('Error data')
+        // }
+        // catch(ex){
+        //     next(ex)
+        // }   
+    }
+))
 
 router.get('/fast', (req, res) => {
     res.send('Fast!')
